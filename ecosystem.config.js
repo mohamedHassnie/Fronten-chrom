d85c74fe2fa86fadbb10d786efc199cac05b2807
mohamed Hassnie <mohamed.benhssin@sesame.com.tn>
2022-03-30 14:50:02 +0000

    v2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,17 +1,22 @@
-module.exports = {
+module.export = {
   apps: [
     {
-      name: "genetic-frontend",
-      script: "serve",
-      args: "-s build 3000",
-      instances: "max",
-      exec_mode: "cluster",
-      env: {
-        NODE_ENV: "development",
-      },
-      env_production: {
-        NODE_ENV: "production",
-      },
+      script: "bin/www",
+      watch: ".",
     },
   ],
+
+  deploy: {
+    production: {
+      user: "admin",
+      host: "10.10.50.24",
+      ref: "origin/master",
+      repo: "https://github.com/mohamedHassnie/Fronten-chrom.git",
+      path: "/home/admin/Fronten-chrom",
+      "pre-deploy-local": "",
+      "post-deploy":
+        "npm install &&  pm2 reload ecosystem.config.js --env production",
+      "pre-setup": "",
+    },
+  },
 };
